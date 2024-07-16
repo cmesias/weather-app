3457554e7854b9a526efa81abea2c1b0c0cf64c0
Carl <carlmesias20@gmail.com>
2024-07-16 13:57:20 -0700

    remove second method
    removed second method because we do not need it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Website API: https://www.weatherapi.com/my/
+// Author: Carl Mesias
 
 function getWeather() {
 
@@ -34,33 +35,3 @@ function getWeather() {
             console.error('Error:', error);
         });
 }
-
-function getWeatherFromLosAngelos() {
-
-    let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=291bee6211ff41fd948193904241607 &q=';
-    apiUrl += 'Los Angeles';
-    apiUrl += '&aqi=no';
-
-    console.log(apiUrl);
-
-    // Make a GET request
-    fetch(apiUrl)
-        .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-
-            document.querySelector(".temperature").innerHTML = data.current.temp_f + "F";
-            document.querySelector(".city").innerHTML = data.location.name;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-// Call this to get weather from Los Angeles upon opening weather app
-getWeatherFromLosAngelos();
